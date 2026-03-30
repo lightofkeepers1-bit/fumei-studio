03ff17a9803984c1bbb3067c9e803b7fa1ba75e5
lightofkeepers1-bit <lightofkeepers1@gmail.com>
2026-03-30 10:33:52 +0800

    Add files via upload

--- api/news.js
+++ api/news.js
@@ -61,6 +61,8 @@ function buildRssUrl(category) {
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-firebase-uid');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
 
